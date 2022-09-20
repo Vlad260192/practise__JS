b7cf3e8808a1446f3974d0c12ee8569344e13f3c
Vlad260192 <v.kapustianskiy@gmail.com>
2022-09-20 17:42:00 +0300

    commit

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
    $('.button').on('click', function(){
-      $('li:nth-child(odd)').slideToggle();
+      $('body').toggleClass('change');
    });
+
+   $('.test-btn').on('click', function(){
+      $('.main__info').each(function(){
+         $(this).children(':nth-child(1)').css('color', 'red');
+         $(this).children(':nth-child(2)').css('color', 'green');
+         $(this).children(':nth-child(3)').css('color', 'yellow');
+      });
+   });
+
+   
+
 });
 
 
