18913e61e1f6138aef35c15ec442ed094643f4d0
Vlad260192 <v.kapustianskiy@gmail.com>
2022-10-11 10:29:46 +0300

    calc commit

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-$(document).ready(function(){
-   $('.button').on('click', function(){
-      $('body').toggleClass('change');
-   });
 
-   $('.test-btn').on('click', function(){
-      $('.main__info').each(function(){
-         $(this).children(':nth-child(1)').css('color', 'red');
-         $(this).children(':nth-child(2)').css('color', 'green');
-         $(this).children(':nth-child(3)').css('color', 'yellow');
-      });
-   });
+function insert(num) {
+   document.form.textview.value = document.form.textview.value + num;
+}
 
-   let w = $('.example').width();
-   let h = $('.example').height();
+function clean() {
+   document.form.textview.value = '';
+}
 
-   alert(h);
+function back() {
+   let exp = document.form.textview.value;
 
-});
+   document.form.textview.value = exp.substring(0, exp.length-1);
+}
 
+function equal() {
+   let exp = document.form.textview.value;
 
+   if (exp) {
+      document.form.textview.value = eval(exp);
+   }
+}
