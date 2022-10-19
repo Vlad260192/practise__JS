8c7fbded56ee1e7ea46077bc52a820159d59adc2
Vlad260192 <v.kapustianskiy@gmail.com>
2022-10-19 18:00:07 +0300

    commit

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+// function doNothing(a, b) {
+//    a = 10;
+//    b = 10;
+   
+//    return a * b;
+
+// }
+
+// alert( doNothing() );
+
+let sayHi = function() {
+   alert('Hello world!');
+}
+
+let func = sayHi;
+
+func();
+
+// sayHi();
