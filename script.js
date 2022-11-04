74f22a2c80b89785873e99108cf9ffdecc18a36f
Vlad260192 <v.kapustianskiy@gmail.com>
2022-11-04 19:05:58 +0200

    commit

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-let userName = prompt(`Hi what's your name?`);
+let greeting = "Hi what's your name?";
+let userName = prompt(greeting);
 
 while (userName == '' || userName == null ) {
 	alert('You need to enter your name before start the game');
-	userName = prompt(`Hi what's your name?`);
+	userName = prompt(greeting);
 } 
 
 alert( userName + ', you need to guese a number. After each try you will get a message: "perfect match", "hot", "cold".');
 
-let guessNumber = Math.ceil( Math.random() * 100);;
+let guessNumber = random(100);
 let guess = prompt('Guess number');
-let numberOfGuesses = 0;
+let numberOfGuesses = 1;
 
 while ( guess != guessNumber ) {
-
+	numberOfGuesses += 1;
+	
 	if ( guess < guessNumber ) {
 		guess = prompt('You was wrong, to cold');
-		numberOfGuesses += 1;	
-	} 
-	
-	if (guess > guessNumber ) {
+	} else {
 		guess = prompt('You was wrong, to hot');
-		numberOfGuesses += 1;
 	}
 }
 
@@ -29,3 +27,4 @@ if ( numberOfGuesses <= 1 ) {
 } else {
 	alert( 'Perfect match, it was ' + guessNumber + '. It took you ' + numberOfGuesses + ' attempts.' );
 }
+
